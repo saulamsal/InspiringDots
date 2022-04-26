4941fcded893c378c5391a5bcbadb2b08da73988
saulamsal <saulamsal@gmail.com>
2022-04-25 21:33:52 -0400

    Added stylings + dropzone

---
+++ script.js
@@ -0,0 +1,12 @@
+message = document.querySelector("[data-message]");
+
+
+// Five locations you can drop the dots.
+// The coords object contains objects with the x & y coords.
+const coords = {
+    dropOne: {},
+    dropTwo: {},
+    dropThree: {},
+    dropFour: {},
+    dropFive: {}
+}
